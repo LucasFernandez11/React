a152ee674e37580b8ecf1f2c606eb8aa8bb79cac
Lucas Fernández <lucas.fernamdez.lmf11@gmail.com>
2022-01-13 13:08:31 -0300

    subiendo App.js
    subiendo desafio

--- ecommers-fernandez/src/App.js
+++ ecommers-fernandez/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hola Rodri! que tengas una buena semana, y teneme paciencia porfa!!!
         </a>
       </header>
     </div>
